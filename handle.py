b7181f6ee5873e55c050228f2d6f8c293b0edb76
zhyjor <zhyjor@163.com>
2017-08-31 19:05:04 +0800

    fixed:add token

--- handle.py
+++ handle.py
@@ -14,7 +14,7 @@ class Handle(object):
             timestamp = data.timestamp
             nonce = data.nonce
             echostr = data.echostr
-            token = "xxxx" #请按照公众平台官网\基本配置中信息填写
+            token = "zhyjor2017" #请按照公众平台官网\基本配置中信息填写
 
             list = [token, timestamp, nonce]
             list.sort()
