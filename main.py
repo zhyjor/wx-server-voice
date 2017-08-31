c38c243ea816cb370500ab3fca469edeb72fe538
zhyjor <zhyjor@163.com>
2017-08-31 18:56:40 +0800

    add:handle

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 # filename: main.py
 import web
+from handle import Handle
 
 urls = (
     '/wx', 'Handle',
 )
-
-class Handle(object):
-    def GET(self):
-        return "hello, this is a test"
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
