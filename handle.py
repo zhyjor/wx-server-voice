584e97ec2abe5d1fc5f6f03c52b14bce56600c03
zhyjor <zhyjor@163.com>
2017-08-31 20:08:46 +0800

    add:menu

--- handle.py
+++ handle.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # filename: handle.py
-import hashlib
 import reply
 import receive
 import web
@@ -11,14 +10,62 @@ class Handle(object):
             webData = web.data()
             print "Handle Post webdata is ", webData   #后台打日志
             recMsg = receive.parse_xml(webData)
-            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+            if isinstance(recMsg, receive.Msg):
                 toUser = recMsg.FromUserName
                 fromUser = recMsg.ToUserName
-                content = "test"
-                replyMsg = reply.TextMsg(toUser, fromUser, content)
-                return replyMsg.send()
-            else:
-                print "暂且不处理"
-                return "success"
+                if recMsg.MsgType == 'text':
+                    content = "test"
+                    replyMsg = reply.TextMsg(toUser, fromUser, content)
+                    return replyMsg.send()
+                if recMsg.MsgType == 'image':
+                    mediaId = recMsg.MediaId
+                    replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
+                    return replyMsg.send()
+            if isinstance(recMsg, receive.EventMsg):
+                if recMsg.Event == 'CLICK':
+                    if recMsg.Eventkey == 'mpGuide':
+                        content = u"编写中，尚未完成".encode('utf-8')
+                        replyMsg = reply.TextMsg(toUser, fromUser, content)
+                        return replyMsg.send()
+            print "暂且不处理"
+            return reply.Msg().send()
         except Exception, Argment:
             return Argment
+  2）vim receive.py (修改)
+# -*- coding: utf-8 -*-
+# filename: receive.py
+import xml.etree.ElementTree as ET
+
+def parse_xml(web_data):
+    if len(web_data) == 0:
+        return None
+    xmlData = ET.fromstring(web_data)
+    msg_type = xmlData.find('MsgType').text
+    if msg_type == 'event':
+        event_type = xmlData.find('Event').text
+        if event_type == 'CLICK':
+            return Click(xmlData)
+        #elif event_type in ('subscribe', 'unsubscribe'):
+            #return Subscribe(xmlData)
+        #elif event_type == 'VIEW':
+            #return View(xmlData)
+        #elif event_type == 'LOCATION':
+            #return LocationEvent(xmlData)
+        #elif event_type == 'SCAN':
+            #return Scan(xmlData)
+    elif msg_type == 'text':
+        return TextMsg(xmlData)
+    elif msg_type == 'image':
+        return ImageMsg(xmlData)
+
+class EventMsg(object):
+    def __init__(self, xmlData):
+        self.ToUserName = xmlData.find('ToUserName').text
+        self.FromUserName = xmlData.find('FromUserName').text
+        self.CreateTime = xmlData.find('CreateTime').text
+        self.MsgType = xmlData.find('MsgType').text
+        self.Event = xmlData.find('Event').text
+class Click(EventMsg):
+    def __init__(self, xmlData):
+        EventMsg.__init__(self, xmlData)
+        self.Eventkey = xmlData.find('EventKey').text
